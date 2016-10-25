815e77d5d3dcb872ac929a6448e6c591e74cf82f
Ali Murtaza <shahalimurtaza@gmail.com>
2016-10-25 14:37:52 -0700

    Completed Exercise2.py

--- Week2/Exercise2.py
+++ Week2/Exercise2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
 import telnetlib
-
-#if __name__ == "__main__":
-#    main()
-
+import time
 
 ip_addr = '184.105.247.70'
 username = 'pyclass'
@@ -13,33 +10,30 @@ password = '88newclass'
 TELNET_PORT = 23
 TELNET_TIMEOUT = 6
 
-main()
 
 def main():
     remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
 
     output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
-    print output
-
-    remote_conn.write('pyclass' + '\n')
+    remote_conn.write(username + '\n')
 
     output = remote_conn.read_until("ssword:", TELNET_TIMEOUT)
+    remote_conn.write(password + '\n')
+    
+    time.sleep(1)
+    
+    output = remote_conn.read_very_eager()
+    
     print output
-    remote_conn.write('88newclass' + '\n')
-
-    #remote_conn.read_until(<string_pattern>, TELNET_TIMEOUT)
+    
     remote_conn.write('show ip int br' + '\n')
+    
+    time.sleep(1)
 
     output = remote_conn.read_very_eager()
-
     print output
     
     remote_conn.close()
 
-  
-#output = remote_conn.read_very_eager()
-
-#remote_conn.close()
-
-#print output
-
+if __name__ == "__main__":
+    main()
